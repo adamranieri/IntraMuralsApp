64655b9a86ebdf84f955dc4d3253d32beb965aa7
Skye Gibney <skyejgibney@gmail.com>
2023-01-11 13:07:42 -0800

    Added UserManagerPage to page object models

---
+++ src/test/java/dev/jsx/pages/UserManagerPage.java
@@ -0,0 +1,38 @@
+package dev.jsx.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class UserManagerPage {
+    private WebDriver driver;
+
+    public UserManagerPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public WebElement getRowByUserId(String id) {
+        List<WebElement> rows = userTableBody.findElements(By.xpath("./tr"));
+
+        for (int i = 0; i < rows.size(); i++) {
+            WebElement userIdCell = rows.get(i).findElement(By.xpath("./td[1]"));
+
+            if (userIdCell.getText() == id) {
+                return rows.get(i);
+            }
+        }
+
+        return null;
+    }
+
+    @FindBy(id="userTableBody")
+    WebElement userTableBody;
+
+    @FindBy(linkText="Back")
+    WebElement backLink;
+}
